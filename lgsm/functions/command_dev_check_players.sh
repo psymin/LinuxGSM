8ce2cb1c6b56602da5919691cd51ef7e1a380fcc
psymin <psymin@runlinux.net>
2019-08-27 14:57:17 +0000

    chmod

---
+++ lgsm/functions/command_dev_check_players.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# LinuxGSM check_status.sh function
+# Author: Daniel Gibbs
+# Contributor: UltimateByte
+# Website: https://linuxgsm.com
+# Description: Checks the process status of the server. Either online or offline.
+
+local commandname="CHECK"
+local function_selfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+if [ "${shortname}" == "wf" ]; then
+
+fi
