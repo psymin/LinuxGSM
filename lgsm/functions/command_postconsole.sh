f09dc27dd695bc6e1582193c8441a651745fce35
psymin <psymin@runlinux.net>
2019-08-28 15:23:47 +0000

    postdetails fixes

--- lgsm/functions/command_postconsole.sh
+++ lgsm/functions/command_postconsole.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# LinuxGSM command_postdetails.sh function
-# Author: CedarLUG
-# Contributor: CedarLUG
+# LinuxGSM command_postconsole.sh function
+# Author: psymin
+# Contributor: psymin
 # Website: https://linuxgsm.com
-# Description: Strips sensitive information out of Details output
+# Description: Like postconsole log but for console log
 
-local commandname="POSTDETAILS"
-local commandaction="Postdetails"
+local commandname="POSTCONSOLE"
+local commandaction="Postconsole"
 local function_selfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 # Set posttarget to the appropriately-defined post destination.
@@ -34,51 +34,10 @@ posttarget=${posttarget="https://hastebin.com"}
 # This, too, may be overridden from the command line at the top-level.
 postexpire="${postexpire="30D"}"
 
-# source all of the functions defined in the details command.
-info_messages.sh
-
-fn_bad_postdetailslog() {
-	fn_print_fail_nl "Unable to create temporary file ${postdetailslog}."
-	core_exit.sh
-}
-
-# Remove any existing postdetails.log file.
-if [ -f "${postdetailslog}" ]; then
-	rm -f "${postdetailslog}"
-fi
-
-# Rather than a one-pass sed parser, default to using a temporary directory.
-if [ -n "${alertflag}" ]; then
-	postdetailslog="${alertlog}"
-else
-	# Run checks and gathers details to display.
-	check.sh
-	info_config.sh
-	info_parms.sh
-	info_distro.sh
-	info_messages.sh
-	query_gamedig.sh
-	touch "${postdetailslog}" || fn_bad_postdetailslog
-	{
-		fn_info_message_distro
-		fn_info_message_performance
-		fn_info_message_disk
-		fn_info_message_gameserver
-		fn_info_message_script
-		fn_info_message_backup
-		# Some game servers do not have parms.
-		if [ "${shortname}" != "ts3" ]&&[ "${shortname}" != "jc2" ]&&[ "${shortname}" != "jc3" ]&&[ "${shortname}" != "dst" ]&&[ "${shortname}" != "pz" ]&&[ "${engine}" != "renderware" ]; then
-			fn_parms
-			fn_info_message_commandlineparms
-		fi
-		fn_info_message_ports
-		fn_info_message_select_engine
-		fn_info_message_statusbottom
-	} | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"| tee -a "${postdetailslog}" > /dev/null 2>&1
-fi
+postconsolelog="log/console/wfserver-console.log"
 
 if [ "${posttarget}" == "http://pastebin.com" ] ; then
-	fn_print_dots "Posting details to pastbin.com for ${postexpire}"
+	fn_print_dots "Posting console to pastbin.com for ${postexpire}"
 	# grab the return from 'value' from an initial visit to pastebin.
 	csrftoken=$(${curlpath} -s "${posttarget}" |
 					sed -n 's/^.*input type="hidden" name="csrf_token_post" value="\(.*\)".*$/\1/p')
@@ -89,30 +48,31 @@ if [ "${posttarget}" == "http://pastebin.com" ] ; then
 				-F "post_key=${csrftoken}" -F "paste_expire_date=${postexpire}" \
 				-F "paste_name=${gamename} Debug Info" \
 				-F "paste_format=8" -F "paste_private=0" \
-				-F "paste_type=bash" -F "paste_code=<${postdetailslog}" |
+				-F "paste_type=bash" -F "paste_code=<${postconsolelog}" |
 				awk '/^location: / { print $2 }' | sed "s/\n//g")
 
 	 # Output the resulting link.
-	fn_print_ok_nl "Posting details to pastbin.com for ${postexpire}"
+	fn_print_ok_nl "Posting console to pastbin.com for ${postexpire}"
 	pdurl="${posttarget}${link}"
 	echo "  Please share the following url for support: ${pdurl}"
 elif [ "${posttarget}" == "https://hastebin.com" ] ; then
-	fn_print_dots "Posting details to hastebin.com"
+	fn_print_dots "Posting console to hastebin.com"
 	# hastebin is a bit simpler.  If successful, the returned result
 	# should look like: {"something":"key"}, putting the reference that
 	# we need in "key".  TODO - error handling. -CedarLUG
-	link=$(${curlpath} -H "HTTP_X_REQUESTED_WITH:XMLHttpRequest" -s -d "$(<${postdetailslog})" "${posttarget}/documents" | cut -d\" -f4)
-	fn_print_ok_nl "Posting details to hastebin.com for ${postexpire}"
+	link=$(${curlpath} -H "HTTP_X_REQUESTED_WITH:XMLHttpRequest" -s -d "$(<${postconsolelog})" "${posttarget}/documents" | cut -d\" -f4)
+	fn_print_ok_nl "Posting console to hastebin.com for ${postexpire}"
 	pdurl="${posttarget}/${link}"
 	echo "  Please share the following url for support: ${pdurl}"
 elif [ "${posttarget}" == "https://termbin.com" ] ; then
-	fn_print_dots "Posting details to termbin.com"
-	link=$(cat "${postdetailslog}" | nc termbin.com 9999 | tr -d '\n\0')
-	fn_print_ok_nl "Posting details to termbin.com"
+	fn_print_dots "Posting console log to termbin.com"
+	# the sed is to remove ANSI colors
+	link=$(cat "${postconsolelog}" | sed 's/\x1b\[[0-9;]*m//g' | nc termbin.com 9999 | tr -d '\n\0')
+	fn_print_ok_nl "Posting console log to termbin.com"
 	pdurl="${link}"
 	echo "  Please share the following url for support: ${pdurl}"
 else
-	 fn_print_warn_nl "Review output in: ${postdetailslog}"
+	 fn_print_warn_nl "Review console log in: ${postconsolelog}"
 	 core_exit.sh
 fi
 
