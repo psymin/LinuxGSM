819cdfb60448553ca02233aebdfbe5c198a6c68c
psymin <psymin@runlinux.net>
2019-08-31 17:50:26 +0000

    changing to only use gamedig for now

--- lgsm/functions/command_safe_update.sh
+++ lgsm/functions/command_safe_update.sh
@@ -15,9 +15,10 @@ check_ip.sh
 # both methods work with wfserver
 # set here for verifying and testing
 # preferring gsquery for reduced deps
+# edit: changing to gamedig only for merging to warfork-stable
 
-querymethod="gsquery"
-#querymethod="gamedig"
+#querymethod="gsquery"
+querymethod="gamedig"
 
 check_status.sh
 if [ "${status}" != "0" ]; then
@@ -29,9 +30,18 @@ if [ "${status}" != "0" ]; then
                 if [ ! -f "${functionsdir}/query_gsquery.py" ]; then
                         fn_fetch_file_github "lgsm/functions" "query_gsquery.py" "${functionsdir}" "chmodx" "norun" "noforce" "nomd5"
                 fi
-                clients=$(${functionsdir}/query_gsquery.py -a "${ip}" -p "${queryport}" -e "${engine}" | sed -e s/^.*clients..// | sed -e s/.\n.*//)
+		gs_command="${functionsdir}/query_gsquery.py -a \"${ip}\" -p \"${queryport}\" -e \"${engine}\""
+		#gs_output_uniform="${gs_command} 2> /dev/null | sed -e 's/\\\\/\\/g' | sed -e 's/^.*statusResponse.*challenge..//' | sed -e 's/^.challenge..//' | grep -v status | sed -e s/.n\'$//"
+		gsarray=$("${gs_command} 2> /dev/null | sed -e 's/\\\\/\\/g' | sed -e 's/^.*statusResponse.*challenge..//' | sed -e 's/^.challenge..//' | grep -v status | sed -e s/.n\'$//")
+
+		declare -A gsq_result
+		
+#		clients=$(${gs_command} | sed -e s/^.*clients..// | sed -e s/.\n.*//)
+#                clients=$(${functionsdir}/query_gsquery.py -a "${ip}" -p "${queryport}" -e "${engine}" | sed -e s/^.*clients..// | sed -e s/.\n.*//)
 		players="${clients}"
                 querystatus="$?"
+		echo "gscmd:	${gs_command}"
+		echo "unif:	${gs_output_uniform}"
 	fi
 
 	if [ "${players}" == "0" ]; then
